ebc5ccd1caaff56899d9ba80644fcee8393efbdc
Raymond An <raymond_an@yahoo.com>
2023-03-19 23:27:42 -0400

    met all deliverables

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,23 @@
 class Animal:
-    pass
+
+    all = []
+
+    def __init__(self, species, weight, nickname, zoo):
+        self.species = species
+        self.weight = weight
+        self.nickname = nickname
+        self.zoo = zoo
+        Animal.all.append(self)
+
+    @classmethod
+    def find_by_species(cls, species):
+        filtered = []
+        for animal in cls.all:
+            if animal.species == species:
+                filtered.append(animal)
+        return filtered
+
+
+
+
+
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,50 @@
-from lib.animal import Animal
+from animal import Animal
+import ipdb
+class Zoo(Animal):
 
-class Zoo:
-    pass
+    all = []
+
+    def __init__(self, name="", location=""):
+        self.name = name
+        self.location = location
+        Zoo.all.append(self)
+
+    def animals(self):
+        return Animal.all
+    
+    def animal_species(self):
+        species = []
+        for animal in Animal.all:
+            if animal.species not in species:
+                species.append(animal.species)
+        return species
+    
+    def find_by_species(self, species):
+        animal_list = []
+        for animal in Animal.all:
+            if animal.species == species:
+                animal_list.append(animal)
+        return animal_list
+    
+    def animal_nicknames(self):
+        nickname_list = []
+        for animal in Animal.all:
+            nickname_list.append(animal.nickname)
+        return nickname_list
+
+    @classmethod
+    def find_by_location(cls, location):
+        filtered = []
+        for zoo in cls.all:
+            if zoo.location == location:
+                filtered.append(zoo)
+        return filtered
+
+
+z1 = Zoo( 'Micke Grove Zoo', 'Lodi, CA' )
+a1 = Animal( 'Lion', 75, 'Luke', z1 )
+a2 = Animal( 'Dog', 50, 'Ammo', z1 )
+a3 = Animal( 'Dog', 50, 'Cici', z1 )
+
+
+# ipdb.set_trace()
